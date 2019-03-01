ec3ce93271ae105ab4d1f21519e80dd7442d36d0
kanitsharma <kanitshrm09@gmail.com>
2019-03-01 13:51:32 +0530

    Added chain and functor law tests

--- test/index.js
+++ test/index.js
@@ -1,6 +1,42 @@
 import test from 'ava';
 import Effect from '../build/main';
 
+test.cb('Functor Law, fmap id = id', t => {
+  const id = x => x;
+  const normal = x => Effect.of(id(x));
+  const mapped = x => Effect.of(x).map(id);
+
+  // Assertion
+  normal(10)
+    .chain(x =>
+      mapped(10).map(y => {
+        t.is(x, y);
+        return y;
+      }),
+    )
+    .fork(
+      () => {},
+      () => {
+        t.end();
+      },
+    );
+});
+
+test.cb('Chain Test', t => {
+  Effect((_, resolve) => {
+    resolve('First');
+  })
+    .chain(x =>
+      Effect((_, resolve) => {
+        resolve(`${x} Second`);
+      }),
+    )
+    .fork(null, x => {
+      t.is(x, 'First Second');
+      t.end();
+    });
+});
+
 test.cb('Cancellation check', t => {
   const run = () =>
     Effect(
